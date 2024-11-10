8a2367ed30006500e301efdfc0f154c236e905b5
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-10 15:50:51 +0200

    updated

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+import './App.css'
+
+function App() {
+
+
+  return (
+  
+      <div>
+        <Header />
+        <MainContent />
+        <Footer />
+        <WelcomeMessage />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </div>
+    
+  )
+}
+
+export default App;
